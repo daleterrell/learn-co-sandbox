070c95379390395a63b478f65ce91b9489f21548
Dale Terrell <tragicid@gmail.com>
2018-08-20 20:55:25 +0000

    this stupid thing is working again.

--- lib/practical/practical.rb
+++ lib/practical/practical.rb
@@ -6,24 +6,22 @@ class Practical::Practical
     
     movies << self.scrape_screenrant
     movies << self.scrape_top10hq
+  end
     
     
-    
-    [movie]
-    
-    def self.scrape screenrant
-      doc = Nokogiri::HTML (open(https://screenrant.com/horror-movies-used-practical-effects-no-cgi/))
-    end
+    def self.scrape_screenrant
+      doc = Nokogiri::HTML (open("https://screenrant.com/horror-movies-used-practical-effects-no-cgi/"))
     
     movie = self.new 
-    movie.name = doc.search("h3.main-title")text
+    movie.name = doc.search("h3.main-title").text
+  end
     
-    def self.scrape scrape top10hq
-      doc = Nokogiri::HTML (open(https://www.top10hq.com/top-10-best-make-special-effects-horror-movies/))
-    end
+    def self.scrape_top10hq
+      doc = Nokogiri::HTML (open("https://www.top10hq.com/top-10-best-make-special-effects-horror-movies/"))
+    
     
     movie = self.new
-    movie.name = doc.search("h2.item-title")text
-    
+    movie.name = doc.search("h2.item-title").text
+  
   end
 end
